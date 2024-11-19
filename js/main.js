3ba3a40a685f08c83acd8aad1759408b8d725186
DiegoMiguelM <diegomiranda2007@cesanrafael.org>
2024-11-19 12:22:15 -0400

    proyectojavascrip

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+let mensaje= "Hola Hijos Del Diablo >:v";
+console.log(mensaje);
+
+function sumar(){
+    let numerouno = 3;
+    let numerotres = 4;
+    let sumar= numerouno+numerotres; 
+    alert("la sumar es" + sumar);
+}
+function resta(){
+    let numerouno = 3;
+    let numerotres = 4;
+    let resta= numerouno-numerotres; 
+    alert("la resta es" + resta);
+}
+function multi(){
+    let numerouno = 3;
+    let numerotres = 4;
+    let multi= numerouno*numerotres; 
+    alert("la multi es" + multi);
+}
+function divi(){
+    let numerouno = 3;
+    let numerotres = 4;
+    let divi= numerouno/numerotres; 
+    alert("la divi es" + divi);
+}
+
+
+
